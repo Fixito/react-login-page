498da037e4bf142028690860edffd6282157c8dd
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-03-27 18:54:20 +0100

    Enhance not found middleware to include HTTP method in error response

--- server/middleware/not-found.ts
+++ server/middleware/not-found.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export function notFound(req: Request, res: Response) {
-  res.status(StatusCodes.NOT_FOUND).json({ error: `${req.path} Not Found` });
+  res.status(StatusCodes.NOT_FOUND).json({ error: `${req.method} ${req.path} Not Found` });
 }
